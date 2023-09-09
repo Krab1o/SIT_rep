78771325d3b9d1fc60bcc880e6f503f93800ec33
Krab1o <bogdansils@gmail.com>
2023-09-09 20:30:28 +0400

    Recursive Fibonacci function added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+int fib(int n)
+{
+	if (n == 1)
+		return 1;
+	if (n == 2)
+		return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << fib(10) << std::endl;
 	return 0;
 }
